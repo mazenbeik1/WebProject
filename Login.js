ec96887450a6eb60d9d42eb7ccf352ad7815c8cc
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-11-27 20:02:03 +0300

    nerdView table added

--- Login.js
+++ Login.js
@@ -1,38 +1,31 @@
 let LoginData;
 
-async function getLoginData()
-{
-    let promise = new Promise(function(Resolve)
-    {
-        fetch("Login.json")
-        .then(Response => Response.json())
-        .then(data => {
-            Resolve(data);
-        })
-    })
-    promise.then(
-        function(data){
-            LoginData = data;
-        }
-    );
+async function getLoginData() {
+	let promise = new Promise(function (Resolve) {
+		fetch("Login.json")
+			.then((Response) => Response.json())
+			.then((data) => {
+				Resolve(data);
+			});
+	});
+	promise.then(function (data) {
+		LoginData = data;
+	});
 
-    await promise;
+	await promise;
 }
 
-async function login()
-{
-    let id = document.getElementById("StudentId").value;
-    let password = document.getElementById("Password").value;
-    
-    await getLoginData();
+async function login() {
+	let id = document.getElementById("StudentId").value;
+	let password = document.getElementById("Password").value;
 
-    LoginData.forEach(elem =>
-        {
-            if(elem.id == id && elem.password == password)
-            {
-                localStorage.setItem("UserID",id);
-                localStorage.setItem("UserRole",elem.role);
-                window.location.href=`${elem.role}View.html`;
-            }
-        })
+	await getLoginData();
+
+	LoginData.forEach((elem) => {
+		if (elem.id == id && elem.password == password) {
+			localStorage.setItem("UserID", id);
+			localStorage.setItem("UserRole", elem.role);
+			window.location.href = `${elem.role}View.html`;
+		}
+	});
 }
--- studentView.js
+++ studentView.js
@@ -1,70 +1,74 @@
-let userID= localStorage.getItem("UserID");
+let userID = localStorage.getItem("UserID");
 let userRole = localStorage.getItem("UserRole");
 let studentID = userID;
 let assignmentsData;
 let studentData;
 
-async function requestData()
-{
-    let promise = new Promise(function(Resolve)
-    {
-        fetch("assignments.json")
-        .then(Response => Response.json())
-        .then(data => {
-            Resolve(data);
-        })
-    })
-    promise.then(
-        function(data){
-            assignmentsData = data;
-        }
-    );
-    await promise;
+async function requestData() {
+	let promise = new Promise(function (Resolve) {
+		fetch("assignments.json")
+			.then((Response) => Response.json())
+			.then((data) => {
+				Resolve(data);
+			});
+	});
+	promise.then(function (data) {
+		assignmentsData = data;
+	});
+	await promise;
 }
 
-function requestStudentData()
-{
-    assignmentsData.forEach(element => {
-        if(element.id == studentID)
-        {
-            studentData=element;
-        }
-    })
+function requestStudentData() {
+	assignmentsData.forEach((element) => {
+		if (element.id == studentID) {
+			studentData = element;
+		}
+	});
 }
 
+function render() {
+	if (userRole == "expert" || userRole == "student") {
+		let fName = document.createElement("div");
+		fName.innerHTML = `${studentData.fName} ${studentData.lName}`;
+		document.getElementById("studentView").appendChild(fName);
 
-function render()
-{
-    if(userRole == "expert" || userRole == "student")
-    {
-        let fName = document.createElement("div");
-        fName.innerHTML=`${studentData.fName} ${studentData.lName}`;
-        document.getElementById("studentView").appendChild(fName);
+		let id = document.createElement("div");
+		id.innerHTML = studentID;
+		document.getElementById("studentView").appendChild(id);
 
-        let id = document.createElement("div");
-        id.innerHTML=studentID;
-        document.getElementById("studentView").appendChild(id);
+		let grade = document.createElement("div");
+		grade.innerHTML = `Score: ${studentData.grade}/100`;
+		document.getElementById("studentView").appendChild(grade);
+	}
 
-        let grade = document.createElement("div");
-        grade.innerHTML=`Score: ${studentData.grade}/100`;
-        document.getElementById("studentView").appendChild(grade);
-    }
-    
+	if (userRole == "nerd") {
+		let elem = document.createElement("div");
+		elem.innerHTML = `YOU ARE A NERD. YOU CAN GRADE ME <button onclick="gradeStudent()">Grade</button>`;
 
-    if(userRole == "nerd")
-    {
-        let elem = document.createElement("div");
-        elem.innerHTML="YOU ARE A NERD. YOU CAN GRADE ME <button>Grade</button>";
-        document.getElementById("studentView").appendChild(elem);
-    }
+		document.getElementById("studentView").appendChild(elem);
+	}
 }
 
-async function setup()
-{
-    if(localStorage.getItem("StudentID")){studentID = localStorage.getItem("StudentID")}
-    await requestData();
-    requestStudentData();
-    render();
+function gradeStudent() {
+	let gradeInput = document.createElement("input");
+	document.getElementById("studentView").appendChild(gradeInput);
+
+	let submitGrade = document.createElement("button"); 
+	submitGrade.innerHTML = `Give a Score`;
+	document.getElementById("studentView").appendChild(submitGrade);
+
+	let grade = document.createElement("div");
+	grade.innerHTML = `Score: ${gradeInput.value}/100`;
+	document.getElementById("studentView").appendChild(grade);
 }
 
-setTimeout(setup,100);
+async function setup() {
+	if (localStorage.getItem("StudentID")) {
+		studentID = localStorage.getItem("StudentID");
+	}
+	await requestData();
+	requestStudentData();
+	render();
+}
+
+setTimeout(setup, 100);
