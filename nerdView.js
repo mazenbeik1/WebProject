ed28a28aad3c6e14285f5abdad68c52c4821308f
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-11-27 22:11:58 +0300

    Updating CSS

--- nerdView.js
+++ nerdView.js
@@ -1,72 +1,51 @@
-let userID= localStorage.getItem("UserID");
+let userID = localStorage.getItem("UserID");
 let userRole = localStorage.getItem("UserRole");
 let StudentID = userID;
 let assignmentsData;
 
-function renderTable(assignedStudent)
-{
-    
-    assignmentsData.forEach(assignment =>{
-        if( assignment.assignedNerd == userID)
-        {   
-            if(userRole == "expert")
-            {
-                let elem = document.createElement("tr");
-                elem.innerHTML = `<td>${assignment.id}</td> <td>${assignment.fName}</td> <td>${assignment.lName}</td> <td><button  id="${assignment.id}" onclick="studentView(this.id)"> View </button></td>`
-                document.getElementById("assignmentsTable").appendChild(elem);
-            }else if(userRole == "nerd")
-            {
-                let elem = document.createElement("tr");
-                elem.innerHTML = `<td>******</td> <td>******</td> <td>******</td> <td><button  id="${assignment.id}" onclick="studentView(this.id)"> View </button></td>`
-                document.getElementById("assignmentsTable").appendChild(elem);
-            }
-        }
-    })
+function renderTable(assignedStudent) {
+	assignmentsData.forEach((assignment) => {
+		if (assignment.assignedNerd == userID) {
+			if (userRole == "expert") {
+				let elem = document.createElement("tr");
+				elem.innerHTML = `<td>${assignment.id}</td> <td>${assignment.fName}</td> <td>${assignment.lName}</td> <td><button  id="${assignment.id}" type="button" class="btn btn-info" onclick="studentView(this.id)"> View </button></td>`;
+				document.getElementById("assignmentsTable").appendChild(elem);
+			} else if (userRole == "nerd") {
+				let elem = document.createElement("tr");
+				elem.innerHTML = `<td>******</td> <td>******</td> <td>******</td> <td><button  id="${assignment.id}" onclick="studentView(this.id)"> View </button></td>`;
+				document.getElementById("assignmentsTable").appendChild(elem);
+			}
+		}
+	});
 }
 
+function studentView(id) {
+	id = parseInt(id);
+	window.location.href = "studentView.html";
 
-function studentView(id)
-{
-    id = parseInt(id);
-    window.location.href="studentView.html";
-    
-    localStorage.setItem("StudentID",id);
+	localStorage.setItem("StudentID", id);
 }
 
-async function requestData()
-{
-    let promise = new Promise(function(Resolve)
-    {
-        fetch("assignments.json")
-        .then(Response => Response.json())
-        .then(data => {
-            Resolve(data);
-        })
-    })
-    promise.then(
-        function(data){
-            assignmentsData = data;
-        }
-    );
-    await promise;
+async function requestData() {
+	let promise = new Promise(function (Resolve) {
+		fetch("assignments.json")
+			.then((Response) => Response.json())
+			.then((data) => {
+				Resolve(data);
+			});
+	});
+	promise.then(function (data) {
+		assignmentsData = data;
+	});
+	await promise;
 }
 
-
-
-
-async function setup()
-{
-    if(localStorage.getItem("StudentID")){StudentID = localStorage.getItem("StudentID")}
-    await requestData();
-    renderTable();
+async function setup() {
+	if (localStorage.getItem("StudentID")) {
+		StudentID = localStorage.getItem("StudentID");
+	}
+	await requestData();
+	renderTable();
 }
 
-
-
-
-
-
-
-
-
-setTimeout(setup,100);
+setTimeout(setup, 100);
